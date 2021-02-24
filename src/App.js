4f71f0134375aea981c52422a6229beb0f170f9e
Soham Sarkar <soham.ss1@gmail.com>
2021-02-24 12:59:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import Pokemon from './components/Pokemon'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Pokemon from './components/Pokemon/Pokemon';
+import Header from './components/Navigation/Header';
+import Footer from './components/Navigation/Footer';
+import About from './components/About/About';
 
 function App() {
   return (
-    <>
-      <Pokemon />
-      
-    </>
+    <Router>
+      <Header />
+      <Switch>
+        <Route>
+          <About path="/about" />
+        </Route>
+        <Route exact path="/">
+          <Pokemon />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
--- src/components/utils/PokemonImage.js
+++ src/components/utils/PokemonImage.js
@@ -6,7 +6,7 @@ const url='https://pokeapi.co/api/v2/pokemon/';
 export default function PokemonImage({pokeName}) {
     const [image, setImage] = useState([])
 
-    axios.get(`${url}/pokeName`)
+    axios.get(`${url}/${pokeName}`)
         .then(res=>{
             setImage(res.data.sprites.front_default)
         })
