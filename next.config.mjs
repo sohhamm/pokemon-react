1846bb1a713fa0f0c63f69bd5d5ad74ac0abd399
Soham Sarkar <soham.ss1@gmail.com>
2025-07-13 13:34:11 +0530

    chore: remove unused deps

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,22 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/PokeAPI/sprites/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.pokemon.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
